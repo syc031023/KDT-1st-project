91254095b701748e012ab8120d275e11b77dd4a2
echong22 <tdc8165@naver.com>
2024-10-02 12:37:53 +0900

    메인페이지 작업완료 데이터 구현완료

--- calendar/send.js
+++
@@ -1,7 +0,0 @@
-    // 보낼 데이터
-    const data = 'data';
-
-    const aTag = document.querySelector('a');
-    aTag.addEventListener('click', () => {
-      location.href = `/calendar/detail.html?${data}`
-});
