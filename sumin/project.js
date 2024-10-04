673d71c375f8a08932d2f8f85a094a8aa6927034
sumin1124 <bonoksm1124@naver.com>
2024-10-04 14:02:37 +0900

    Add files via upload

--- sumin/project.js
+++ sumin/project.js
@@ -1,31 +1,32 @@
-/* <하트 누르면 색깔 변하고 다시 누르면 색깔 없어지게 하기>
+function updateTimer() {
+  const future = Date.parse("2024/10/05 13:00:00");
+  const now = new Date();
+  const diff = future - now;
 
-const 변수명 = document.querySelector("#~") //id명
-변수명.addEventListener("click", ( ) =>{
-    let isOn = wrap.classList.contains("on");
-    console.log(isOn);
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const mins = Math.floor(diff / (1000 * 60));
+  const secs = Math.floor(diff / 1000);
 
-    if(isOn){
-       wrap.classList.remove("on");
-    }else{
-       wrap.classList.add("on");
-    }
-});
-
-
-* 더 간단한 방법 ( 삼항 연산자 )
-
-  (isOn) ? wrap.classList.remove("on") : wrap.classList.add("on");
-
-
-* 더더 간단한 방법( toggle )
-  wrap.classList.toggle("on"); */
-
-{
-  /* < 커서 올리면 미우스 모양 바뀌게 하는 법>
- js 파일
- 태그명.클래스명{
-    cursor: pointer;(한쪽만 가르키는 손가락)
- } 
-    cursor: grab;(지도 길찾기사진에 올리면 나타나는 다편 손가락)*/
+  const d = days;
+  const h = hours - days * 24;
+  const m = mins - hours * 60;
+  const s = secs - mins * 60;
+  document.getElementById("timer").innerHTML =
+    "<div>" +
+    "<div class='txt-box'>DAY</div>" +
+    d +
+    "<span>Days</span></div>" +
+    "<div>" +
+    "<div class='txt-box'>HOUR</div>" +
+    h +
+    "<span>Hours</span></div>" +
+    "<div>" +
+    "<div class='txt-box'>MIN</div>" +
+    m +
+    "<span>Minutes</span></div>" +
+    "<div>" +
+    "<div class='txt-box'>SEC</div>" +
+    s +
+    "<span>Seconds</span></div>";
 }
